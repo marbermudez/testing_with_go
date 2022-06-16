d1ecc4fac2473b2d83190d7eeb935c7251880623
marbermudez <martin.bermudez@mercadolibre.com>
2022-06-16 12:22:25 -0300

    Testeo de Sum con slices

--- integer/integers.go
+++ integer/integers.go
@@ -5,10 +5,11 @@ func Add(x, y int) int {
 	return x + y
 }
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers{
 		sum += number
 	}
 	return sum
 }
+
