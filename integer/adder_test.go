b41052ec8eb0af4325793a06c9bc76545dce2139
marbermudez <martin.bermudez@mercadolibre.com>
2022-06-16 12:14:08 -0300

    Agregado de modulo de iteracion + suma de arrays

--- integer/adder_test.go
+++ integer/adder_test.go
@@ -19,3 +19,15 @@ func ExampleAdd() {
 	fmt.Println(sum)
 	//Output: 6
 }
+
+func TestSum(t *testing.T) {
+
+	numbers := [5]int{1,2,3,4,5}
+
+	got := Sum(numbers)
+	want := 15
+
+	if got != want{
+		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
+}
--- integer/integers.go
+++ integer/integers.go
@@ -4,3 +4,11 @@ package integer
 func Add(x, y int) int {
 	return x + y
 }
+
+func Sum(numbers [5]int) int {
+	sum := 0
+	for _, number := range numbers{
+		sum += number
+	}
+	return sum
+}
